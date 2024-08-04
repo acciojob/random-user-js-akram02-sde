5836fbd9537e79470bca399b3910ae5929a4ba29
Akram Ahmed Khan <68415682+akram02-sde@users.noreply.github.com>
2024-08-04 23:16:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,39 +1,20 @@
 //your code here
-let userImage = document.getElementById("userImg");
-let userName = document.getElementById("userName");
-let aditionalInfo = document.getElementById("info");
-let ageBtn = document.getElementById("age");
-let emailBtn = document.getElementById("email");
-let phBtn = document.getElementById("ph");
-let fetchDataBtn = document.getElementById("getUser");
-//fetching data from API and displaying it on the webpage using async function
-
-let baseUrl = "https://randomuser.me/api/";
 async function getData(){
-    let response = await fetch(baseUrl);
-    let mockResponseOne = await response.json();
-    console.log(mockResponseOne.results[0]);
-    //changing userName
-    const fullName = `${mockResponseOne.results[0].name.first} ${mockResponseOne.results[0].name.last}`;
-	userName.innerText = fullName;
-
-    // console.log(data.results[0].picture.large);
-    //changing userImage
-    userImage.src = `${mockResponseOne.results[0].picture.large}`
-
-    aditionalInfo.innerText = "";
-    ageBtn.addEventListener("click", (e)=>{
-        // console.log("Age: ",data.results[0].dob.age);
-        aditionalInfo.innerText = `${mockResponseOne.results[0].dob.age}`
+    const response = await fetch('https://randomuser.me/api/')
+    const result = await response.json()
+    const userData = result.results[0]
+    console.log(userData)
+    photo.src = userData.picture.large
+    info.innerText = ''
+    age.addEventListener('click', e=>{
+        info.innerText = userData.dob.age
     })
-    emailBtn.addEventListener("click", ()=>{
-        // console.log(data.results[0].email);
-        aditionalInfo.innerText = `${mockResponseOne.results[0].email}`
+    email.addEventListener('click', e=>{
+        info.innerText = userData.email
     })
-    phBtn.addEventListener("click",()=>{
-        // console.log(data.results[0].phone);
-        aditionalInfo.innerText = `${mockResponseOne.results[0].phone}`;
+    phone.addEventListener('click', e=>{
+        info.innerText = userData.cell
     })
-    
 }
-fetchDataBtn.addEventListener("click", getData);
+
+getData()
